3e568e4fcc7f133e4ede47e1edad609491b913d2
nithin9000 <nithinkrishnalal303@gmail.com>
2024-11-12 19:00:34 +0530

    all the files

---
+++ frontend/src/app.config.server.ts
@@ -0,0 +1,11 @@
+// src/app/app.config.server.ts
+import { ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering()
+  ]
+};
+
+export const config = serverConfig;
