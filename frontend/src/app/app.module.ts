1383f0261fdb9b79d591e49877e4ad3e6a7ef20f
nithin9000 <nithinkrishnalal303@gmail.com>
2024-11-12 16:04:08 +0530

    routes

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+	MatCarouselModule,
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
