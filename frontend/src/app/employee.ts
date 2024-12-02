a9fd6a0ce3012c939a9ae3d76887f8cc1da23cac
nithin9000 <nithinkrishnalal303@gmail.com>
2024-12-02 13:35:45 +0530

    Last commit

--- frontend/src/app/employee.ts
+++ frontend/src/app/employee.ts
@@ -20,12 +20,4 @@ export class Employee {
     this.salary=0;
     this.department="";
     this.designation="";
-    // this.joiningDate = new Date();
-  //  // Set the default date value
-  //  const today = new Date();
-  //  const year = today.getFullYear();
-  //  const month = ('0' + (today.getMonth() + 1)).slice(-2); // Adding 1 because months are zero-based
-  //  const day = ('0' + today.getDate()).slice(-2);
-
-  //  this.joiningDate = `${year}-${month}-${day}`;
 }}
